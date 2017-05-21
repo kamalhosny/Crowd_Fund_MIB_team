cf9b4c3c235c3af31572403894c6443106767334
hadi fayed <hadifayed@gmail.com>
2017-05-21 10:46:44 +0200

    add error responses and user refrence to campaign

--- app/controllers/campaign_controller.rb
+++ app/controllers/campaign_controller.rb
@@ -1,28 +1,43 @@
 class CampaignController < ApplicationController
 
-  def create
-    @campaign = current_user.campaigns.create campaign_params
+  def index
+    campaigns = Campaign.all
     respond_to do |format|
-      format.json  { render :json => @campaign }
+      format.json { render :json => campaigns}
+    end
+  end
+
+  def create
+    campaign = current_user.campaigns.new campaign_params
+    respond_to do |format|
+      if campaign.save!
+        format.json {render :json => campaign}
+      else
+        format.json {render campaign.errors.full_messages.to_json, status: 400 }
+      end
     end
   end
 
   def destroy
-    @campaign = Campaign.find(params[:id])
-    @campaign.destroy
+    campaign = Campaign.find(params[:id])
+    campaign.destroy
   end
 
   def update
-    @campaign = Campaign.find(params[:id]).update
+    campaign = Campaign.find(params[:id])
     respond_to do |format|
-      format.json  { render :json => @campaign }
+      if campaign.update! campaign_params
+        format.json {render :json => campaign}
+      else
+        format.json {render campaign.errors.full_messages.to_json, status: 400}  }
+      end
     end
   end
 
   def show
-    @campaign = Campaign.find(params[:id])
+    campaign = Campaign.find(params[:id])
     respond_to do |format|
-      format.json  { render :json => @campaign }
+      format.json  { render :json => campaign }
     end
   end
 

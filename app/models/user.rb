2fde1aa2ae1adcb6ef1e1df84470d30867ce82f7
Saleh <salehaly01@gmail.com>
2017-05-23 15:09:35 +0200

    Add paperclip to upload user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,21 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :omniauthable
-
+  :recoverable, :rememberable, :trackable, :validatable,
+  :omniauthable
+  
   has_many :comments
   has_many :campaigns
+  has_attached_file :image,
+  :styles => {large: "1000x1000>", medium: "300x300>", thumb: "100x100#"},
+  default_url: "/images/:style/missing.png"
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 
   include DeviseTokenAuth::Concerns::User
-  
+
   validates :username,presence: true
 
 end

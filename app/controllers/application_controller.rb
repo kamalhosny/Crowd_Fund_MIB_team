7f0e9a8a6a972a0c8a0985076994a7a9c6adebcb
Kamal <kamal-aldin-hosny@hotmail.com>
2017-05-25 15:14:31 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,9 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   # include DeviseTokenAuth::Concerns::SetAdminByToken
   include ActionController::MimeResponds
-  #
-  #
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   respond_to :json
-
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age, :gender, :credit_card])
   end

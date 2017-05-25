6489253ce4da111a27910006dbab041f69c9eca8
hadi fayed <hadifayed@gmail.com>
2017-05-25 13:16:38 +0200

    fix pull request comment

--- spec/controllers/campaign_controller_spec.rb
+++ spec/controllers/campaign_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe CampaignController, type: :controller do
   context 'listing' do
-    it "should return all the campaigns in the database" do
-      
+    xit "should return all the campaigns in the database" do
+
     end
   end
 

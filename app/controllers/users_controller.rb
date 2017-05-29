0bf9b274ba5f929ad2900f85f733b06cad8f3817
Saleh <salehaly01@gmail.com>
2017-05-29 10:05:28 +0200

    Add user delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_action: authenticate_member!, only: [:index]
+  skip_before_action: authenticate_member!, only: [:show]
+
   def show
     user = User.find(params[:id])
     respond_to do |format|
@@ -9,6 +10,17 @@ class UsersController < ApplicationController
 
   def update
     current_user.update!(user_params)
+  end
+
+  def destroy
+    user=User.find params[:id]
+    respond_to do |format|
+      if user.delete!
+        format.json {render json: {message: "user: '#{params[:id]}' deleted"}, status: 200}
+      else
+        format.json {render user.errors.full_messages.to_json, status: 400}
+      end
+    end
   end
 
   private

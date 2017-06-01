5355043051954ab92ec0e3114b7a4c2255205a79
Saleh <salehaly01@gmail.com>
2017-06-01 18:37:13 +0200

    Fix user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  skip_before_action :authenticate_member!, only: [:show, :update]
+  skip_before_action :authenticate_member!, only: [:show]
 
   def show
     user = User.find(params[:id])

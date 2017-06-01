12846f4b0090073e4bc596602cfc2075d237b09f
Saleh <salehaly01@gmail.com>
2017-06-01 18:34:58 +0200

    Allow update in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  skip_before_action :authenticate_member!, only: [:show]
+  skip_before_action :authenticate_member!, only: [:show, :update]
 
   def show
     user = User.find(params[:id])
